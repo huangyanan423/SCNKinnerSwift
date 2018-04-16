f8215af68f7c6a80641d6ef02fca50cd7cf7129e
指尖流年 <huangyanan423@163.com>
2018-04-16 15:17:18 +0800

    添加按钮

--- SCNKinnerSwift/SCNKinnerSwift/ViewController.swift
+++ SCNKinnerSwift/SCNKinnerSwift/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         
        setUpGameView()
+       setChangeClourButton()
         
     }
     
@@ -77,6 +78,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
          //8.将动画组添加到模型节点上
         let node = scnView.scene?.rootNode.childNode(withName: "node", recursively: true)
         node?.addAnimation(animationGroup, forKey: "animation")
+        
+    }
+    
+    func setChangeClourButton() {
+        //白色衣服
+        let whiteBtn = UIButton.init(type: UIButtonType.custom)
+        whiteBtn.frame = CGRect.init(x: 20, y: 60, width: 100, height: 40)
+        whiteBtn.setTitle("白色衣服", for: UIControlState.normal)
+        whiteBtn.backgroundColor = UIColor.red
+        whiteBtn.addTarget(self, action: #selector(whiteButtonClick), for: .touchUpInside)
+        self.view.addSubview(whiteBtn)
+        
+        //紫色衣服
+        let purpleBtn = UIButton.init(type: UIButtonType.custom)
+        purpleBtn.frame = CGRect.init(x: 20, y: 120, width: 100, height: 40)
+        purpleBtn.setTitle("紫色衣服", for: UIControlState.normal)
+        purpleBtn.backgroundColor = UIColor.red
+        purpleBtn.addTarget(self, action: #selector(purpleButtonClick), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(purpleBtn)
+        
+        //❤️衣服
+        let heartBtn = UIButton.init(type: UIButtonType.custom)
+        heartBtn.frame = CGRect.init(x: 20, y: 180, width: 100, height: 40)
+        heartBtn.setTitle("❤️衣服", for: UIControlState.normal)
+        heartBtn.backgroundColor = UIColor.red
+        heartBtn.addTarget(self, action: #selector(heartButtonClick), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(heartBtn)
+    }
+    
+    //白色衣服
+    @objc func whiteButtonClick(_sender:UIButton) {
+        
+        let shirtNode = scnView.scene?.rootNode.childNode(withName: "shirt", recursively: true)
+        shirtNode?.geometry?.firstMaterial?.diffuse.contents = "export_0_texture20.png";
+        
+    }
+    
+    //紫色衣服
+    @objc func purpleButtonClick(_sender:UIButton)  {
+        
+        let shirtNode = scnView.scene?.rootNode.childNode(withName: "shirt", recursively: true)
+        shirtNode?.geometry?.firstMaterial?.diffuse.contents = "export_0_texture19.png";
+        
+    }
+    
+    //❤️衣服
+    @objc func heartButtonClick(_sender:UIButton){
+        
+        let shirtNode = scnView.scene?.rootNode.childNode(withName: "shirt", recursively: true)
+        shirtNode?.geometry?.firstMaterial?.diffuse.contents = "export_0_texture22.png";
         
     }
     
